756223b17961be46bba59ea97f9ab50251968857
Scott Macdonald <scott.scotm@gmail.com>
2022-02-08 21:44:40 +0000

    Tighten up type-safety.

--- components/ExerciseList.tsx
+++ components/ExerciseList.tsx
@@ -2,17 +2,28 @@ import * as React from "react";
 import ExerciseAdditional from "./ExerciseAdditional";
 import { PencilAltIcon } from "@heroicons/react/outline";
 import Link from "next/link";
+import { Equipment, Exercise, ExerciseBaseData, Muscles } from "@prisma/client";
+
+interface MyExerciseBaseData extends ExerciseBaseData {
+  muscles?: Muscles[];
+  equipment?: Equipment[];
+}
+
+interface MyExercise extends Exercise {
+  id: number;
+  exercise_base?: MyExerciseBaseData;
+}
 
 export interface IExerciseListProps {
-  items: any[];
+  exercises: Exercise[];
 }
 
 export default function ExerciseList(props: IExerciseListProps) {
-  const { items } = props;
+  const exercises = props.exercises as MyExercise[];
   return (
     <>
-      {items.length > 0 ? (
-        items.map((e) => {
+      {exercises.length > 0 ? (
+        exercises.map((e) => {
           return (
             <div key={e.id} className="p-4">
               <h3 className="text-3xl font-bold">{e.name}</h3>
@@ -24,14 +35,22 @@ export default function ExerciseList(props: IExerciseListProps) {
               <div>
                 <div dangerouslySetInnerHTML={{ __html: e.description }}></div>
                 <hr />
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-3 gap-4">
                   <ExerciseAdditional
                     precursor="Muscles Used: "
-                    named={e.exercise_base.muscles}
+                    named={e?.exercise_base?.muscles}
                   ></ExerciseAdditional>
                   <ExerciseAdditional
                     precursor="Equipment: "
-                    named={e.exercise_base.equipment}
+                    named={e?.exercise_base?.equipment}
+                  ></ExerciseAdditional>
+                  <ExerciseAdditional
+                    precursor="Author: "
+                    named={
+                      e?.exercise_base?.license_author !== undefined
+                        ? [{ name: e?.exercise_base?.license_author }]
+                        : undefined
+                    }
                   ></ExerciseAdditional>
                 </div>
               </div>
