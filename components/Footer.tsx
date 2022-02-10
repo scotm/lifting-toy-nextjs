b1d619d996fab0b4f25744e79c2b966827412b83
Scott Macdonald <scott.scotm@gmail.com>
2022-02-10 00:22:03 +0000

    Fleshing out the Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import * as React from "react";
 
 export interface IFooterProps {}
@@ -6,11 +7,22 @@ export default function Footer(props: IFooterProps) {
   return (
     <footer className="m-2 grid grid-cols-3 gap-x-8 gap-y-4">
       <div className="rounded-lg bg-red-700 p-2 text-white">
-        <h4>About this software</h4>
-        <p></p>
+        <h4 className="text-xl font-bold">About this software</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas illo,
+          expedita amet, tempora, minima possimus debitis eos quidem adipisci
+          ducimus eligendi voluptatum exercitationem harum pariatur saepe
+          perspiciatis sint et inventore!
+        </p>
+        <p>
+          Latest source code can be{" "}
+          <Link href="https://github.com/scotm/lifting-toy-nextjs">
+            found on GitHub.
+          </Link>
+        </p>
       </div>
       <div className="rounded-lg bg-red-700 p-2 text-white">
-        <h4>Making heavy things move is good</h4>
+        <h4 className="text-xl font-bold">Resistance Training</h4>
         <p>
           Regular resistance training can decrease the risk of heart disease by
           lowering body fat, decreasing blood pressure, improving cholesterol,
@@ -19,7 +31,7 @@ export default function Footer(props: IFooterProps) {
         </p>
       </div>
       <div className="rounded-lg bg-red-700 p-2 text-white">
-        <h4>Keep track of yer gainz</h4>
+        <h4 className="text-xl font-bold">Tracking yer gainz</h4>
         <p>
           That which gets measured gets improved. Nothing motivates us more than
           seeing our progress.
